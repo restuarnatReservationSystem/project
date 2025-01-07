4cce67f8b83bd316600cd94d88eefb9a2c9ad989
Lee <fttc100@gmail.com>
2025-01-07 14:11:04 +0900

    loginProcess

--- page/login/login_process.php
+++ page/login/login_process.php
@@ -3,8 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <title>로그인 시스템</title>
+        <link href="../login/login_process.css" rel="stylesheet" type="text/css" />
     </head>
-    <body>
+    <body>  
+    </body>
         <?php
             $uid = $_POST['uid'];
             $upw = $_POST['upw'];
@@ -19,13 +21,21 @@
             
 
             if ($result) {
-                echo "로그인 성공! 환영합니다.".$_SESSION['nickname'];
-                echo "<br>3초 후 가게 페이지로 이동합니다.<br>";
+                echo "<div id='main'>";
+                echo "<div id='header'>";
+                echo "<h1>".$_SESSION['nickname']."님 환영합니다."."</h1>";
+                echo "<h2>3초 후 가게 페이지로 이동합니다.</h2>";
                 header("Refresh: 1; url='../main/main.php'");
+                echo "</div>";
+                echo "</div>";
+                
             } else {
-                echo "아이디 또는 비밀번호가 틀렸습니다. 다시 시도해주세요.";
-                echo "<br>3초 후 로그인 페이지로 이동합니다.<br>";
-                header("Refresh: 1; url=login.html");
+                echo "<div id='main'>";
+                echo "<div id='header'>";
+                echo "<h2>아이디 또는 비밀번호가 틀렸습니다. 다시 시도해주세요.</h2>";
+                echo "<h2>3초 후 로그인 페이지로 이동합니다.</h2>";
+                echo "</div>";
+                echo "</div>";
             }
         ?>
     </body>
