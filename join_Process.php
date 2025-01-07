e268e5a97611f2508be29f9ac53de2a1f7de0d6b
godjaehee01 <ljh9236032@naver.com>
2025-01-08 02:57:24 +0900

    Add files via upload

---
+++ join_Process.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>회원 가입 시스템</title>
+    </head>
+    <body>
+        <?php
+        $uid = $_POST['uid'];
+        $upw = $_POST['upw'];
+        $nickname = $_POST['nickname'];
+        $p_num = $_POST['p_num'];
+
+        include '../model/userJoin.php';
+
+        userJoin($uid, $upw, $nickname, $p_num);
+        ?>
+    </body>
+</html>
